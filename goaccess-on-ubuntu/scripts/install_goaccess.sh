6403e46466291087261bde22e8c2a04ef57099d6
Pravesh Jindal <prjindal@microsoft.com>
2018-09-07 18:30:41 +0530

    First revision for the Azure quickstart Templates for the installing GoAccess Log Analyzer

---
+++ goaccess-on-ubuntu/scripts/install_goaccess.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Add Official GoAccess Repository
+echo "deb http://deb.goaccess.io/ $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/goaccess.list
+wget -O - https://deb.goaccess.io/gnugpg.key | sudo apt-key add -
+
+# Update Packages 
+apt-get -y update
+
+# Install Apache2
+apt-get -y install apache2
+
+# write some HTML
+echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
+
+# restart Apache
+apachectl restart
+
+# Install GoAccess
+apt-get install goaccess
+
+
+
+
+
