c2f703c51318dc27bfce442e2c77e72da94a8cdd
Pravesh Jindal <prjindal@microsoft.com>
2018-09-10 09:41:48 +0530

    Updating the Install Script for dotnetcore

--- goaccess-on-ubuntu/scripts/setup_vm.sh
+++ goaccess-on-ubuntu/scripts/setup_vm.sh
@@ -6,7 +6,12 @@ echo $1 >> /usr/share/appgw/blobsasuri.key
 chmod 644 /usr/share/appgw/blobsasuri.key
 
 # Install .Net Core current version
-sh dotnet-install.sh -c Current
+# sh dotnet-install.sh -c Current
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+apt-get install apt-transport-https
+apt-get update
+apt-get install aspnetcore-runtime-2.1
 
 # Install and Setup GoAccess
 sh install_goaccess.sh
