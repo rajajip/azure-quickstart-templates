e8c047f881c8845204dd67d612b1a27e444a8c12
Pravesh Jindal <prjindal@microsoft.com>
2018-09-11 19:46:34 +0530

    Editing the Metadata

--- application-gateway-logviewer-goaccess/scripts/install_goaccess.sh
+++ application-gateway-logviewer-goaccess/scripts/install_goaccess.sh
@@ -19,8 +19,8 @@ apt-get -y install apache2
 # Install GoAccess
 apt-get -y install goaccess
 
-# Start GoAccess
-goaccess /var/log/azure/Microsoft.Azure.Networking.ApplicationGateway.LogProcessor/access_log.log -o /var/www/html/report.html --real-time-html --port=8080 --log-format='"%^": "%dT%t+%^","%^": {%^=>%^, %^=>"%h", %^=>%^, %^=>"%m", %^=>"%U", %^=>"%q", %^=>"%u", %^=>"%s", %^=>"%H", %^=>"%b", %^=>%^, %^=>"%T", %^=>%^},' --time-format='%T' --date-format='%Y-%m-%d' &
-
 # restart Apache
 apachectl restart
+
+# Start GoAccess
+goaccess /var/log/azure/Microsoft.Azure.Networking.ApplicationGateway.LogProcessor/access_log.log -o /var/www/html/report.html --real-time-html --port=8080 --log-format='"%^": "%dT%t+%^","%^": {%^=>%^, %^=>"%h", %^=>%^, %^=>"%m", %^=>"%U", %^=>"%q", %^=>"%u", %^=>"%s", %^=>"%H", %^=>"%b", %^=>%^, %^=>"%T", %^=>%^},' --time-format='%T' --date-format='%Y-%m-%d' &
